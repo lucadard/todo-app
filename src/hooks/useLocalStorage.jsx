2cf677c5b323c0615802c6482b8615c67a74c056
lucadard <lucadardenne@hotmail.com>
2023-05-06 17:33:52 -0300

    remove console log

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -14,7 +14,6 @@ export function useLocalStorage (key, initialValue) {
     try {
       setStoredValue(value)
       window.localStorage.setItem(key, JSON.stringify(value))
-      console.log('saved to localstorage')
     } catch (err) {
       console.log(err)
     }
