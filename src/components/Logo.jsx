854416c9858c226ff27d0f2faeab654587f4ffaf
lucadard <lucadardenne@hotmail.com>
2023-05-06 18:04:07 -0300

    fix title styles

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,12 @@ import { Box, Text } from '@chakra-ui/react'
 function Logo () {
   return (
     <Box id='Logo' fontSize='2rem'>
-      <Text>T O D O</Text>
+      <Text
+        fontWeight='bold'
+        fontSize='1.2em'
+        letterSpacing={10}
+      >TODO
+      </Text>
     </Box>
   )
 }
